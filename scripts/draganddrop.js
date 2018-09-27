05594ed7a1b28464806e322b3e8bf3e363762f34
NBignall <nathan.bignall@gmail.com>
2018-09-27 09:55:27 -0500

    first pass at drag and drop

---
+++ scripts/draganddrop.js
@@ -0,0 +1,43 @@
+function addDnDHandlers() {
+  var coffeeimages = document.getElementsByClassName("productarticlewide");
+
+  var shoppingCartDropzone = document.getElementbyId("shoppingcart");
+  var shoppingcart = document.querySelectorAll("#shoppingcart ul")[0];
+
+  for (var i = 0; i < coffeeimages.length; i++) {
+    coffeeimages[i].addEventListener("dragstart", function (ev) {
+      ev.dataTransfer.effectAllowed = 'copy';
+      ev.dataTransfer.setData("Text", this.getAttribute("id"));
+    }, false);
+  }
+
+  shoppingCartDropzone.addEventListener("dragover", Function(ev) {
+    if (ev.preventDefault) {
+      ev.preventDefault();
+    }
+    ev.dataTransfer.dropEffect = "copy";
+    return false;
+  }, false);
+
+  shoppingCartDropzone.addEventListener("drop", function (ev) {
+    if (ev.stopPropagation) {
+      ev.stopPropagation();
+    }
+
+    var coffeeId = ev.dataTransfer.getData("Text");
+    var element = document.getElementbyId(coffeeId);
+
+    addCoffeeToShoppingCart(element, coffeeId);
+    ev.stopPropagation();
+
+    return false;
+  }, false);
+
+  function addCoffeeToShoppingCart(item, id) {
+    var html = id + " " + item.getAttribute("data-price");
+
+    var liElement = document.createElement('li');
+    liElement.innerHTML = html;
+    shoppingcart.appendChild(liElement);
+  }
+}
